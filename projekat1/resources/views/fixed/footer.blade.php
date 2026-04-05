17e2774605aa46612908661f3d256bd908eed373
Dušan <127887854+dusankldj@users.noreply.github.com>
2026-04-05 11:41:33 +0200

    Delete projekat1 directory

--- projekat1/resources/views/fixed/footer.blade.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-?>
-
-<footer class="bg-gray-900 text-white py-6">
-    <div class="max-w-7xl mx-auto px-4 text-center">
-        <p>Copyright © Shop {{ date('Y') }}</p>
-    </div>
-</footer>
